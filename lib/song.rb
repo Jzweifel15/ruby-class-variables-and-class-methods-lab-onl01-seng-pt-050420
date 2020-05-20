ca4ab947d3f58421f7ebe266f3ebcfc2dfcf5d2a
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-20 16:26:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,10 @@ class Song
   attr_accessor :name, :artist, :genre 
   attr_reader :name, :artist, :genre
   
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
